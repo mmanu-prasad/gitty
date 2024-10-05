4011a3351c4e927efb51d8e1deeebf779f94e7d1
ManuPrasad <gcpmanuprasad@gmail.com>
2024-10-05 19:48:31 +0530

    groovy file

--- form.groovy
+++ form.groovy
@@ -1,35 +1,16 @@
-job('example-job-with-params') {
-    parameters {
-        activeChoiceParam('PARAM1') {
-            description('First parameter')
-            choiceType('SINGLE_SELECT')
-            groovyScript {
-                script('["Option 1", "Option 2", "Option 3"]')
-                fallbackScript('["Option 1"]')
-            }
+pipelineJob('job-dsl-plugin') {
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('https://github.com/mmanu-prasad/gitty.git')
+          }
+          branch('*/main')
+          scriptPath('Jenkinsfile')
         }
-        activeChoiceParam('PARAM2') {
-            description('Second parameter')
-            choiceType('SINGLE_SELECT')
-            groovyScript {
-                script('["Option A", "Option B", "Option C"]')
-                fallbackScript('["Option A"]')
-            }
-        }
-        activeChoiceParam('PARAM3') {
-            description('Third parameter')
-            choiceType('SINGLE_SELECT')
-            groovyScript {
-                script('["Choice X", "Choice Y", "Choice Z"]')
-                fallbackScript('["Choice X"]')
-            }
-        }
+      }
+     
     }
-    steps {
-        shell('''
-            echo "PARAM1: ${PARAM1}"
-            echo "PARAM2: ${PARAM2}"
-            echo "PARAM3: ${PARAM3}"
-        ''')
-    }
+  }
 }
