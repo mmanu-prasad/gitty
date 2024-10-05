187f0da75c0c1b63b163057a5dbe1b86229de87c
ManuPrasad <gcpmanuprasad@gmail.com>
2024-10-05 17:19:22 +0530

    groovy file

---
+++ form.groovy
@@ -0,0 +1,15 @@
+pipelineJob('job-dsl-plugin') {
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('https://github.com/mmanu-prasad/gitty.git')
+          }
+          branch('*/main')
+        }
+      }
+      lightweight()
+    }
+  }
+}
