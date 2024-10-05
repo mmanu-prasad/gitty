7b85b7eac637a9fb6e04a0565ea3f96ba351425e
ManuPrasad <gcpmanuprasad@gmail.com>
2024-10-05 19:16:01 +0530

    groovy file

--- form.groovy
+++ form.groovy
@@ -1,13 +1,35 @@
-job('example') {
+job('example-job-with-params') {
     parameters {
-        activeChoiceParam('CHOICE-1') {
-            description('Allows user choose from multiple choices')
-            filterable()
+        activeChoiceParam('PARAM1') {
+            description('First parameter')
             choiceType('SINGLE_SELECT')
             groovyScript {
-                script('["choice1", "choice2"]')
-                fallbackScript('"fallback choice"')
+                script('["Option 1", "Option 2", "Option 3"]')
+                fallbackScript('["Option 1"]')
             }
         }
+        activeChoiceParam('PARAM2') {
+            description('Second parameter')
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('["Option A", "Option B", "Option C"]')
+                fallbackScript('["Option A"]')
+            }
+        }
+        activeChoiceParam('PARAM3') {
+            description('Third parameter')
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('["Choice X", "Choice Y", "Choice Z"]')
+                fallbackScript('["Choice X"]')
+            }
+        }
+    }
+    steps {
+        shell('''
+            echo "PARAM1: ${PARAM1}"
+            echo "PARAM2: ${PARAM2}"
+            echo "PARAM3: ${PARAM3}"
+        ''')
     }
 }
